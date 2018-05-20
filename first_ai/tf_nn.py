5865e9ecbe9a6de6b84f8c16b4e60ad151909659
Tyler-Pearson <tylerpearson1993@gmail.com>
2018-05-19 19:44:45 -0700

    first_ai - model plays cartpole

--- first_ai/tf_nn.py
+++ first_ai/tf_nn.py
@@ -92,6 +92,38 @@ def train_model(training_data, model=False):
 training_data = initial_population()
 model = train_model(training_data)
 
+#model.save('myModel.model')
+#model.load('myModel.model')
+
+
+#
+# Play game with model
+#
+scores = []
+choices = []
+
+for each_game in range(10):
+   score = 0
+   game_memory = []
+   prev_obs = []
+   env.reset()
+   for _ in range(goal_steps):
+      env.render()
+      if len(prev_obs) == 0:
+         action = env.action_space.sample()
+      else:
+         action = np.argmax(model.predict(prev_obs.reshape(-1, len(prev_obs), 1))[0])
+      choices.append(action)
+      new_observation, reward, done, info = env.step(action)
+      prev_obs = new_observation
+      game_memory.append([new_observation, action])
+      score += reward
+      if done:
+         break
+   scores.append(score)
+
+print("Average Score: ", sum(scores)/len(scores))
+print("Choice 1: {}, Choice 0: {}".format(float(choices.count(1))/float(len(choices)), float(choices.count(0))/float(len(choices))))
 
 
 
