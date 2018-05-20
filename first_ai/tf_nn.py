e7fc38bf1c0ad0be7fbc91f1ff1492f40165384f
Tyler-Pearson <tylerpearson1993@gmail.com>
2018-05-19 19:21:29 -0700

    first_ai - train model for cartpole

--- first_ai/tf_nn.py
+++ first_ai/tf_nn.py
@@ -57,17 +57,40 @@ def initial_population():
             training_data.append([data[0], output])
       env.reset()
       scores.append(score)
-
    training_data_save = np.array(training_data)
    np.save("saved.npy", training_data_save)
    print("Average accepted score:", mean(accepted_scores))
    print("Median accepted score: ", median(accepted_scores))
    print(Counter(accepted_scores))
-
    return training_data
 
-initial_population()
+def neural_network_model(input_size):
+   network = input_data(shape=[None, input_size, 1], name='input')
+   network = fully_connected(network, 128, activation='relu')
+   network = dropout(network, 0.8)
+   network = fully_connected(network, 256, activation='relu')
+   network = dropout(network, 0.8)
+   network = fully_connected(network, 512, activation='relu')
+   network = dropout(network, 0.8)
+   network = fully_connected(network, 256, activation='relu')
+   network = dropout(network, 0.8)
+   network = fully_connected(network, 128, activation='relu')
+   network = dropout(network, 0.8)
+   network = fully_connected(network, 2, activation='softmax') #output layer
+   network = regression(network, optimizer='adam', learning_rate=LR, loss='categorical_crossentropy', name='targets')
+   model = tflearn.DNN(network, tensorboard_dir='log')
+   return model
 
+def train_model(training_data, model=False):
+   X = np.array([i[0] for i in training_data]).reshape(-1, len(training_data[0][0]), 1)
+   y = [i[1] for i in training_data]
+   if not model:
+      model = neural_network_model(input_size = len(X[0]))
+   model.fit({'input':X}, {'targets':y}, n_epoch=5, snapshot_step=500, show_metric=True, run_id='openaistuff')
+   return model
+
+training_data = initial_population()
+model = train_model(training_data)
 
 
 
